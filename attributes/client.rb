6df6c877444ded0620befd640b0e1071fbe3f7b0
Evgeniy Vasilev <evgeniy@boxore.com>
2013-02-18 15:43:34 +0200

    Added check_http command to client; Added list of monitoring servers as config;

--- attributes/client.rb
+++ attributes/client.rb
@@ -69,3 +69,5 @@ default['nagios']['checks']['load']['warning']    = '15,10,5'
 default['nagios']['checks']['smtp_host'] = String.new
 
 default['nagios']['server_role'] = 'monitoring'
+
+default['nagios']['checks']['http_host'] = 'google.com'
